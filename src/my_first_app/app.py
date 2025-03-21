866880a725c09a0d81002dacd3a802e14e4f8f7e
Dmitri Donskoy <crooper22@gmail.com>
2025-03-21 12:34:26 +0200

    file_check

--- src/my_first_app/app.py
+++ src/my_first_app/app.py
@@ -12,4 +12,15 @@ def index():
 def hello(name):
     return f'Hello {name}'
 
+@app.route('/check_file/<file_name>')
+def check_file(file_name):
+    try:
+        with open(file_name) as file:
+            data = file.readlines()
+        return data
+    except FileNotFoundError:
+        return f'{file_name} not found'
+
+
+
 app.run(host='0.0.0.0' , port=8000 , debug=True)
